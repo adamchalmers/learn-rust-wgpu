60db71d3e10dec8e1f2ae1d7d5c10e7f342c623f
Adam Chalmers <adam.s.chalmers@gmail.com>
2023-03-10 15:08:12 -0600

    Vertex and fragment shader

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,7 @@ struct State {
     size: winit::dpi::PhysicalSize<u32>,
     window: Window,
     color: wgpu::Color,
+    render_pipeline: wgpu::RenderPipeline,
 }
 
 impl State {
@@ -95,6 +96,70 @@ impl State {
         };
         surface.configure(&device, &config);
 
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("Shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        });
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                // What type of vertices you're going to pass to the vertex shader.
+                // OUr shader specifies the vertices in its definition, so this is unnecessary.
+                buffers: &[],
+            },
+            // Stores color data in the `surface`.
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                // What colour outputs it should set up.
+                targets: &[
+                    // We only need one colour output, the `surface`.
+                    Some(wgpu::ColorTargetState {
+                        format: config.format,
+                        // Replace old pixel data with new data. I guess other alternatives would be
+                        // 'blend them together' somehow.
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    }),
+                ],
+            }),
+            primitive: wgpu::PrimitiveState {
+                // i.e. every 3 vertices corresponds to one triangle.
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                // How wgpu should tell if a given triangle is facing forwards or not.
+                // CCW means it's facing forwards if vertices are arranged counter-clockwise.
+                front_face: wgpu::FrontFace::Ccw,
+                // What to cull (i.e. not draw). Anything facing backwards.
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                // How many samples the pipeline will use
+                count: 1,
+                // Which samples should be active? All of them.
+                mask: !0,
+                // For antialiasing.
+                alpha_to_coverage_enabled: false,
+            },
+            // How many array layers the render attachments can have. Not using this.
+            multiview: None,
+        });
+
         Self {
             window,
             surface,
@@ -103,6 +168,7 @@ impl State {
             config,
             size,
             color: BLUE,
+            render_pipeline,
         }
     }
 
@@ -150,7 +216,7 @@ impl State {
         // Clear the screen. Start a new block so that begin_render_pass (which holds &mut)
         // is dropped so that encoder can finish.
         {
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
                 // Describe where to draw the color to.
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
@@ -166,6 +232,10 @@ impl State {
                 })],
                 depth_stencil_attachment: None,
             });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            // Draw something with 3 vertices and 1 instance.
+            render_pass.draw(0..3, 0..1);
         }
 
         // Submit the cmdbuf to the GPU.
---
+++ src/shader.wgsl
@@ -0,0 +1,26 @@
+// Vertex shader
+
+struct VertexOutput {
+    // The annotation tells WGPU that this is the vertex's "clip coordinates".
+    @builtin(position) clip_position: vec4<f32>,
+};
+
+@vertex
+fn vs_main(
+    @builtin(vertex_index) in_vertex_index: u32,
+) -> VertexOutput {
+    var out: VertexOutput;
+    let x = f32(1 - i32(in_vertex_index)) * 0.5;
+    let y = f32(i32(in_vertex_index & 1u) * 2 - 1) * 0.5;
+    out.clip_position = vec4<f32>(x, y, 0.0, 1.0);
+    return out;
+}
+
+// Fragment shader
+
+// location(0) tells WGPU to store the return value in "the first color target"???
+@fragment
+fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> { 
+    // Set color of current fragment to brown.
+    return vec4<f32>(0.3, 0.2, 0.1, 1.0);
+}
